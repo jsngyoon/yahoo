9e93366bd9a619fe89ef265b0baeac521e012056
jsngyoon <jsngyoon@gmail.com>
2020-05-01 21:23:50 +0900

    Add 52wkHighChg

--- yahoo.py
+++ yahoo.py
@@ -19,73 +19,91 @@ def calc_52wkratio(df):
     df['52wkHigh'] = df['High'].asfreq('D').rolling(window=52*7, min_periods=1).max()
     df['52wkLow'] = df['Low'].asfreq('D').rolling(window=52*7, min_periods=1).min()
     df['52wkRatio'] = (df['Close']-df['52wkLow'])/(df['52wkHigh']-df['52wkLow'])*100
+    df['52wkHighChg'] = (df['Close']-df['52wkHigh'])/df['52wkHigh']*100
+
+
+def concat_sorted_df(df):
+    dfs = df.copy()
+    idx = list(dfs.index)
+    for i in idx:
+        s = dfs.loc[i]
+        s = s.sort_values(ascending=False)
+        idy = list(s.index)
+        k = 0
+        for j in idy:
+            k = k + 1
+            dfs.loc[i, j] = k
+    return pd.concat([df, dfs], axis=1)
+
+
+def add_chart(workbook, worksheet, key):
+    chart = workbook.add_chart({'type': 'line'})
+    # bold = workbook.add_format({'bold': 1})
+    for i in range(len(names)):
+        chart.add_series({
+            'name': [key, 0, i + 1],
+            'categories': [key, 1, 0, BACK, 0],
+            'values': [key, 1, i + 1, BACK, i + 1],
+        })
+
+    # Add a chart title and some axis labels.
+    chart.set_title({'name': key + ' History'})
+    chart.set_x_axis({'name': 'Date'})
+    chart.set_y_axis({'name': key + '(%)'})
+
+    # Set an Excel chart style. Colors with white outline and shadow.
+    chart.set_style(10)
+
+    # Insert the chart into the worksheet (with an offset).
+    worksheet.insert_chart('C3', chart, {'x_offset': 25, 'y_offset': 10})
 
 
 # start = datetime(2019,1,1)
 end = datetime.now()
 start = end - timedelta(weeks=WKS)
 df = pd.DataFrame()
+df1 = pd.DataFrame()
 for name, symb in zip(names, symbs):
     globals()[name] = pdr.get_data_yahoo(symb, start, end)
     calc_52wkratio(globals()[name])
     df[name] = globals()[name]['52wkRatio']
+    df1[name] = globals()[name]['52wkHighChg']
 
-df = df[-1*BACK:]
-dfs = df.copy()
-idx = list(dfs.index)
-for i in idx:
-    s = dfs.loc[i]
-    s = s.sort_values(ascending=False)
-    idy = list(s.index)
-    k=0
-    for j in idy:
-        k = k+1
-        dfs.loc[i,j] = k
+df = concat_sorted_df(df[-1*BACK:])
+df1 = concat_sorted_df(df1[-1*BACK:])
 
-df = pd.concat([df, dfs], axis=1)
 writer = ExcelWriter('corona.xlsx', datetime_format='mmm dd')
 df.to_excel(writer, '52wkRatio')
+df1.to_excel(writer, '52wkHighChg')
 
 workbook = writer.book
-worksheet = writer.sheets['52wkRatio']
-
-chart = workbook.add_chart({'type': 'line'})
-# bold = workbook.add_format({'bold': 1})
-# Configure second series. Note use of alternative syntax to define ranges.
-for i in range(len(names)):
-    chart.add_series({
-        'name':       ['52wkRatio', 0, i+1],
-        'categories': ['52wkRatio', 1, 0, BACK, 0],
-        'values':     ['52wkRatio', 1, i+1, BACK, i+1],
-    })
-
-# Add a chart title and some axis labels.
-chart.set_title ({'name': '52wkRatio History'})
-chart.set_x_axis({'name': 'Date'})
-chart.set_y_axis({'name': '52wkRatio(%)'})
-
-# Set an Excel chart style. Colors with white outline and shadow.
-chart.set_style(10)
-
-# Insert the chart into the worksheet (with an offset).
-worksheet.insert_chart('C3', chart, {'x_offset': 25, 'y_offset': 10})
-
+add_chart(workbook,  writer.sheets['52wkRatio'], '52wkRatio')
+add_chart(workbook,  writer.sheets['52wkHighChg'], '52wkHighChg')
 workbook.close()
 #writer.save()
 
 df['DATE'] = df.index.date
 df.set_index('DATE', drop=True, inplace=True)
+df1['DATE'] = df1.index.date
+df1.set_index('DATE', drop=True, inplace=True)
 scope = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
 creds = ServiceAccountCredentials.from_json_keyfile_name('Yahoo-3018bb168e80.json', scope)
 client = gspread.authorize(creds)
-sheet = client.open('Corona').sheet1
-sheet.clear()
-set_with_dataframe(sheet, df, include_index=True)
+sh = client.open('Corona')
+sh.del_worksheet(sh.get_worksheet(1))
+sh.del_worksheet(sh.get_worksheet(1))
+worksheet = sh.add_worksheet(title='52wkRatio', rows=str(2*BACK), cols=str(3*len(name)))
+set_with_dataframe(worksheet, df, include_index=True)
+worksheet = sh.add_worksheet(title='52wkHighChg', rows=str(2*BACK), cols=str(3*len(name)))
+set_with_dataframe(worksheet, df1, include_index=True)
 
 gc = pygsheets.authorize(service_file='Yahoo-3018bb168e80.json')
-sheet = gc.open('Corona').sheet1
 data = []
 for i in range(len(names)):
     data.append(((1,i+2), (BACK,i+2)))
-chart = pygsheets.Chart(sheet, ((1,1), (BACK,1)), ranges=data, chart_type=pygsheets.ChartType.LINE, title='52wkRatio History',anchor_cell='B2')
+sh = gc.open('Corona')
+worksheet = sh[1]
+chart = pygsheets.Chart(worksheet, ((1,1), (BACK,1)), ranges=data, chart_type=pygsheets.ChartType.LINE, title='52wkRatio History',anchor_cell='B2')
+worksheet = sh[2]
+chart = pygsheets.Chart(worksheet, ((1,1), (BACK,1)), ranges=data, chart_type=pygsheets.ChartType.LINE, title='52wkHighChg History',anchor_cell='B2')
 
